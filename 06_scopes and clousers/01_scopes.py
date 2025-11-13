f6afdf3015213a81bf480565bae5cdad38482921
Mohammad Shahid <www.shahid6516@gmail.com>
2025-11-14 00:40:25 +0530

    Scopes and closures in python

---
+++ 06_scopes and clousers/01_scopes.py
@@ -0,0 +1,26 @@
+# username = "Shahid"
+
+# def func():
+#     username = "chai"
+#     print(username)
+# print(username)
+# func()
+
+x = 99
+
+def func2(y):
+    z = x + y
+    return z
+result = func2(1)
+# print(result)
+
+
+def f1():
+    x = 88
+    def f2():
+        print(x)
+    f2()
+f1()
+
+
+print("Hello")
