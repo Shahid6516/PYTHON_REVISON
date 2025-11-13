e2373cd35018ecf3f8f64682765420043cf588bc
Mohammad Shahid <www.shahid6516@gmail.com>
2025-11-13 15:19:01 +0530

    Solution 5 done✅

--- 02_conditionals/05_solution.py
+++ 02_conditionals/05_solution.py
@@ -1,8 +1,10 @@
 wether = input("Please input wether: ")
 
 if wether == "sunny":
-    print("Go for a walk")
+    activity = "Go for a walk"
 elif wether == "rainy":
-    print("Read a book")
+    activity = "Read a book"
 elif wether == "snowy":
-    print("Build a snowman ")
+    activity = "Build a snowman "
+
+print(activity) 
