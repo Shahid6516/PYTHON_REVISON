f95f7a283cf6e7b18668785d7b81a10b21065a71
Mohammad Shahid <www.shahid6516@gmail.com>
2025-11-13 20:23:49 +0530

    05_solution done ✅

--- 03_loops/05_solution.py
+++ 03_loops/05_solution.py
@@ -1,5 +1,6 @@
-   input_str = "teeter"
+input_str = "teeter"
 
-
-   for char in input_str:
-    
+for char in input_str:
+    if input_str.count(char) == 1:
+        print("Char is :", char)
+        break
---
+++ 03_loops/06_solution.py
@@ -0,0 +1,6 @@
+# factorial of 5 = 5*4*3*2*1
+# factorial of 6 = 6*5*4*3*2*1
+
+# Problem: Compute the factorial of a number using a while loop.
+
+
