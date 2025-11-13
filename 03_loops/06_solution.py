d24dca2eca84caa30e2ca981149aa040bd26cff3
Mohammad Shahid <www.shahid6516@gmail.com>
2025-11-13 20:33:28 +0530

    06_solution done✅

--- 03_loops/06_solution.py
+++ 03_loops/06_solution.py
@@ -1,6 +1,16 @@
-# factorial of 5 = 5*4*3*2*1
-# factorial of 6 = 6*5*4*3*2*1
+# factorial of 5 = 5*4*3*2*1 = 120
+# factorial of 6 = 6*5*4*3*2*1 = 720
 
 # Problem: Compute the factorial of a number using a while loop.
+
+number = 5
+factorial = 1
+
+while number > 0:
+    # factorial = factorial*number 
+    factorial *= number
+    number -= 1
+
+print("Factorial value of number is:",factorial)
 
 
