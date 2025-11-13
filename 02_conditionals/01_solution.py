9483d8c885b9d6e7e931b7434f3b28bd9200843b
Mohammad Shahid <www.shahid6516@gmail.com>
2025-11-13 14:03:34 +0530

    Solution 1 done ✔

---
+++ 02_conditionals/01_solution.py
@@ -0,0 +1,14 @@
+age = int(input("Please give your age:"))
+
+if age < 13:
+    print("Child")
+elif age < 20:
+    print("Teenager")  
+elif age < 60:
+    print("Adult")  
+else:
+    print("60+")
+
+
+ 
+ 
