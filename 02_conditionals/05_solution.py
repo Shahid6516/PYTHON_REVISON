d34dbea236f81b371ec60aef5413715c10c48961
Mohammad Shahid <www.shahid6516@gmail.com>
2025-11-13 15:16:21 +0530

    solution 5 done ✅

---
+++ 02_conditionals/05_solution.py
@@ -0,0 +1,8 @@
+wether = input("Please input wether: ")
+
+if wether == "sunny":
+    print("Go for a walk")
+elif wether == "rainy":
+    print("Read a book")
+elif wether == "snowy":
+    print("Build a snowman ")
